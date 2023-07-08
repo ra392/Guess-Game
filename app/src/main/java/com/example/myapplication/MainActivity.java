31537e9c5c03d97265997ca06f8cc3a1439da4e0
ra392 <ra3927538umair@gmail.com>
2023-07-09 00:48:17 +0500

    Main Activity Java

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -14,6 +14,63 @@ import android.view.MenuItem;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle drawerToggle;
+    private NavigationView navigationView;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+
+        // Set up the ActionBarDrawerToggle
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.drawer_open, R.string.drawer_close);
+        drawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        // Set up the navigation item selected listener
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.action_test_game:
+                        // Start GameActivity
+                        Intent gameIntent = new Intent(MainActivity.this, GameActivity.class);
+                        startActivity(gameIntent);
+                        break;
+                    case R.id.action_commits:
+                        // Open a website in a browser
+                        String url = "https://example.com";
+                        Intent websiteIntent = new Intent(Intent.ACTION_VIEW);
+                        websiteIntent.setData(Uri.parse(url));
+                        startActivity(websiteIntent);
+                        break;
+                    // Add more cases for other menu items as needed
+
+                    default:
+                        break;
+                }
+
+                menuItem.setChecked(true);
+                drawerLayout.closeDrawers();
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Toggle the drawer when the hamburger icon is selected
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
 }
